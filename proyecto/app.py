efd671bf350028c293ed0c48d3d0f59fa080e0bd
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2019-03-14 10:23:37 -0600

    [ADD] problema 00

---
+++ proyecto/app.py
@@ -0,0 +1,8 @@
+from Medias import Media
+
+if __name__ == "__main__":
+    medias = Media();
+    vals = [5, 3, 12]
+    print(medias.aritmetica(vals))
+    print(medias.geometrica(vals))
+    print(medias.armonica(vals))
