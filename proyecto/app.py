407d743ed68befd4eed16ed5a4782ea78dedf9f5
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2019-03-19 10:34:50 -0600

    [ADDgst]

--- proyecto/app.py
+++ proyecto/app.py
@@ -3,6 +3,15 @@ from Medias import Media
 if __name__ == "__main__":
     medias = Media();
     vals = [5, 3, 12]
-    print(medias.aritmetica(vals))
-    print(medias.geometrica(vals))
-    print(medias.armonica(vals))
+
+    print('Media aritemetica')
+    print( medias.aritmetica([2,4,8]) )
+    print( medias.aritmetica([1, 5]) )
+    print( medias.aritmetica([1,2,4,8,16,32]) )
+    print('Media geometrica')
+    print( medias.geometrica([1,2,4,8,16,32]) )
+    print( medias.geometrica([0]) )
+    print( medias.geometrica([2,4,8]) )
+    print('Media armonica')
+    print( medias.armonica([2,4,8]) )
+    print( medias.armonica([2,3,6]) )
