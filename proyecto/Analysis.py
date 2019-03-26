f4c1c02fc402f85ac9fed96e562f481efe6828b3
Gerardo Manuel Chavez Larios <gerardo.larios69@gmail.com>
2019-03-26 14:38:38 -0600

    [ADD] activity 03

--- proyecto/Analysis.py
+++ proyecto/Analysis.py
@@ -1,10 +1,19 @@
+import sys
+import json
+import datetime
 import colorama
 from colorama import Fore
-
 class Analysis(object):
     def __init__(self, cases, results):
         self.cases = cases
         self.results = results
+
+    def buid_report(self):
+
+        report_name = datetime.datetime.now().strftime('%c').replace(' ', '-')
+
+        with open(report_name + '.json', 'w') as fp:
+            json.dump(self.results, fp)
 
     def show(self):
         for result in self.results:
@@ -12,4 +21,4 @@ class Analysis(object):
                 print(f'{Fore.RED}caso {result["case_id"]}: Fallido')
             else:
                 print(f'{Fore.GREEN}caso {result["case_id"]}: Exitoso')
-            print(f'Tiempo de ejecución: {result["time_exe"]}')
+            print(f'Tiempo de ejecución: {result["time_exe"]}\n')
--- proyecto/app.py
+++ proyecto/app.py
@@ -3,11 +3,6 @@ from Reader import ReaderCases
 from MethodHandler import Handler
 from Analysis import Analysis
 
-def check_args():
-    print(f'The name of the script: {sys.argv[0]}')
-    print(f'The number of arguments (with the name of the script): {len(sys.argv)}')
-    print(f'The the arguments: {sys.argv}')
-
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         raise Exception('Necesitas agregar los argumentos')
@@ -17,5 +12,6 @@ if __name__ == "__main__":
             handler = Handler(reader.get_cases(), sys.argv[2], sys.argv[3])
             analisys = Analysis(reader.get_cases(), handler.get_results())
             analisys.show()
+            analisys.buid_report()
         except :
             print('Error al procesar los casos de prueba.')
